029a00f9ff751155ed30251877786026f3c9c074
Sixian Lin <davidlin006811@gmail.com>
2019-03-11 18:53:13 -0400

    Change the company name to Iron Rat Customs

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -11,7 +11,7 @@ const NavBar = () => {
             src={Logo}
             width="100"
             height="70"
-            alt="Iron Rat Custom Powder Coating logo"
+            alt="Iron Rat Customs Powder Coating logo"
           />
           I R C
         </p>
