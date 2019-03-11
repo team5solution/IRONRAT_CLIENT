82db3eb4f416b95f2584f4d1f02ade72cce22c49
Sixian Lin <davidlin006811@gmail.com>
2019-03-10 23:38:03 -0400

    1.integrate inde.html to home component; 2. modify webpack.config.js to add support for image loader; 3. Modify React Router; 4. create socketMiddleWare.js to integrate socket.io to Redux as a middleware; 5 Move img folder to scr folder and rename the folder to images; 6. Modify main.css to fix footer incorrect style; 7. create navBar and footer component

---
+++ src/components/careers/career.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class Career extends Component {
+  render() {
+    return <div>Career</div>;
+  }
+}
+export default Career;
--- src/router/Routes.js
+++ src/router/Routes.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "../components/home";
+import About from "../components/about";
+import Contact from "../components/contact";
+import Products from "../components/products/index";
 
 export default () => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
+      <Route path="/products" component={Products} />
     </Switch>
   );
 };
