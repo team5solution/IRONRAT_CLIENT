a47e6803505b78855db96cb248cd37310b131ccb
Sixian Lin <davidlin006811@gmail.com>
2019-03-13 10:55:41 -0400

    add moment.js to deal with the date time

--- src/common/Loading.js
+++ src/common/Loading.js
@@ -2,7 +2,7 @@ import React from "react";
 import Loader from "../images/loader.gif";
 export const Loading = () => (
   <div className="container">
-    <div className="loading-container">
+    <div className="text-center">
       <img src={Loader} alt="Iron Rats Powder Coating loader" />
     </div>
   </div>
