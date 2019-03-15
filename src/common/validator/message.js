ba9d76ef2103d88438f2e4bd597bddc75fad46d5
Sixian Lin <davidlin006811@gmail.com>
2019-03-15 09:25:08 -0400

    fixed the message validation bugs

--- src/common/validator/message.js
+++ src/common/validator/message.js
@@ -5,14 +5,14 @@ export default function messageValidateInput(data) {
   let errors = {};
 
   if (validator.isEmpty(data.name)) {
-    errors.email = "This field is required";
+    errors.name = "This field is required";
   }
 
   if (validator.isEmpty(data.email) || !validator.isEmail(data.email)) {
     errors.email = "This field is required";
   }
   if (validator.isEmpty(data.content)) {
-    errors.description = "This field is required";
+    errors.content = "This field is required";
   }
 
   return {
