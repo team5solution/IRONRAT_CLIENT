105621d34042d862abdb9173e18a4fb3f818a8b1
Mingjiong Liao <liao0019@algonquinlive.com>
2019-03-30 14:56:20 -0400

    integreted modal box into the review page

---
+++ src/components/reviews/reviewBox.js
@@ -0,0 +1,107 @@
+import React, { Component } from "react";
+import Modal from 'react-responsive-modal';
+import Rater from 'react-rater';
+import 'react-rater/lib/react-rater.css';
+
+class ReviewBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      name: "",
+      rating: 0,
+      reviewContent: "",
+      errors: {}
+    };
+  };
+//ewewe
+  onOpenModal = () => {
+    this.setState({ open: true });
+  };
+  onCloseModal = () => {
+    this.setState({ open: false });
+  };
+  
+  passValid = () => {
+    const { errors, isValid } = messageValidateInput(this.state);
+
+    if (!isValid) {
+      this.setState({ errors });
+    }
+
+    return isValid;
+  };
+  onSubmit = e => {
+    //console.log("on submit");
+    e.preventDefault();
+    const isValid = this.passValid();
+    console.log("isValid", isValid);
+    if (isValid) {
+      this.setState({
+        errors: {}
+      });
+      const message = {
+        name: this.state.name,
+        email: this.state.email,
+        content: this.state.content
+      };
+      this.props.sendMessage(message);
+      this.setState({
+        name: "",
+        email: "",
+        content: "",
+        errors: {}
+      });
+    }
+  };
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  render() {
+    const { open } = this.state;
+    return (
+ <div>
+     <hr/>
+        <button onClick={this.onOpenModal} className="btn btn-primary btn-lg" ><h6>Submit a Review</h6></button>
+        <div className="modal fade" >
+            <div className="modal-dialog modal-lg"></div>
+        <div className="modal-content">
+        <Modal open={open} onClose={this.onCloseModal} showCloseIcon={false}>
+                    <div className="modal-header">
+                        <h4 className="modal-title text-dark">Create review</h4>
+                    </div>
+                    <div className="modal-body">
+                    
+                        <div className="form-group mb-5">
+                            <p>
+                                <label className="text-dark" >Name:</label>
+                            </p>
+                            <input  className="form-control" id="name"/>
+                        </div>
+                        <div className="form-group mb-5">
+                            <p>
+                                <label className="text-dark" >Rating:</label>
+                            </p>
+                            <Rater />
+                        </div>
+                        <div className="form-group mb-5">
+                            <p>
+                                <label className="text-dark" >Review:</label>
+                            </p>
+                            <textarea className="form-control" rows="8" id="review"></textarea>
+                        </div>
+
+                        <button className="btn btn-primary"><h6>submit review</h6></button>
+                    </div>
+                    <div className="modal-footer">
+                        <button  className="btn btn-success" onClick={this.onCloseModal}>Close</button>
+                    </div>
+                    </Modal>
+            </div>
+            
+            </div>
+            </div>
+    );
+  }
+}
+export default ReviewBox;
