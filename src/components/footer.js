a13d9568ed7c47977139937514b0073966bd2fef
lapo0163 <lapo0163@users.noreply.github.com>
2019-04-08 22:30:10 -0400

    Updated email address

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,51 +1,35 @@
 import React from "react";
 
-const Footer = () => {
+const Footer = props => {
+  const { bgStyle, linkStyle } = props;
+  let footClass = "bg-dark";
+  if (bgStyle !== null && bgStyle.backgroundColor !== "#000") {
+    footClass = "";
+  }
   return (
-    <div id="footer" className="bg-dark">
+    <div id="footer" className={footClass} style={bgStyle}>
       <div className="text-center">
         <p className="display-4">
-          <a href="https://d.facebook.com/ironratcustoms/">
+          <a href="https://d.facebook.com/ironratcustoms/" style={linkStyle}>
             <i className="fab fa-facebook-square" />
           </a>
-          <a href="https://www.instagram.com/iron_rat_customs/">
+          <a
+            href="https://www.instagram.com/iron_rat_customs/"
+            style={linkStyle}
+          >
             <i className="fab fa-instagram" />
           </a>
-          <a href="mailto:carlsabourin@gmail.com">
+          <a href="mailto:ironratcustom@gmail.com" style={linkStyle}>
             <i className="fas fa-envelope" />
           </a>
-          <a href="tel:+16138623030">
+          <a href="tel:+16138623030" style={linkStyle}>
             <i className="fas fa-phone" />
           </a>
         </p>
       </div>
-      <div className="row mt-4 mb-4">
-        <div className="col-md-4">
-          <p>
-            <a href="https://en.wikipedia.org/wiki/Powder_coating">
-              What is powder coating?
-            </a>
-          </p>
-        </div>
 
-        <div className="col-md-4">
-          <p>
-            <a href="https://www.bencosales.com/metal-strippers/b17-procedures-best-practices-the-bomb-of-strippers/">
-              About Benco B17
-            </a>
-          </p>
-        </div>
-
-        <div className="col-md-4">
-          <p>
-            <a href="http://www.kecocoatings.com/news/cerakote-what-is-it-and-does-it-work-for-you/">
-              About Cerakote
-            </a>
-          </p>
-        </div>
-      </div>
       <div className="text-center text-primary">
-        <p>2018 Copyright</p>
+        <p style={bgStyle}>&copy; 2019 Iron Rat Customs</p>
       </div>
     </div>
   );
